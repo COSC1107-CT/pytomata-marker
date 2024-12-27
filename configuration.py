0b6529091e58fd5841ec1fce33cc6b294c5cc8b2
Harry Porter <h.porter23@icloud.com>
2024-12-27 12:11:56 +1100

    Updated top-level script structure and skeletons

---
+++ configuration.py
@@ -0,0 +1,16 @@
+"""
+Module for question configuration.
+
+Questions are defined using a list of question objects.
+Nested lists denote sub-questions.
+"""
+
+import dataclasses
+
+
+@dataclasses.dataclass
+class Question:
+	pass
+
+
+questions = []
---
+++ run.py
@@ -0,0 +1,8 @@
+"""
+Script that executes the autograder.
+Reads the configuration outlined in `configuration.py`.
+"""
+
+
+if __name__ == "__main__":
+	pass
