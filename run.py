882c38df31ea916132cb0cb012098d4fe834fe32
Harry Porter <h.porter23@icloud.com>
2025-01-03 10:50:27 +1100

    Sets up test grading functions

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 """
-Script that executes the autograder.
-Reads the configuration outlined in `configuration.py`.
+Script for configuring questions and executing the autograder.
 """
+
+# TODO: Enable pointing `run.py` to arbitrary grading scripts.
+import grading
+
+
+def configure_questions():
+    """ """
+    return []
+
+
+def execute_autograder_and_construct_feedback():
+    """ """
+    pass
 
 
 if __name__ == "__main__":
-	pass
+    pass
