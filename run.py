96eacb1c9e79cdc6c2d3e1d2e6e3f04a7077b532
Harry Porter <h.porter23@icloud.com>
2025-01-03 23:43:29 +1100

    Adds basic argument definitions

--- run.py
+++ run.py
@@ -2,13 +2,10 @@
 Script for configuring questions and executing the autograder.
 """
 
-# TODO: Enable pointing `run.py` to arbitrary grading scripts.
-import grading
-
-
-def configure_questions():
-    """ """
-    return []
+# TODO: Accept arbitrary question scripts.
+import questions
+import argparse
+import pathlib
 
 
 def execute_autograder_and_construct_feedback():
@@ -16,5 +13,35 @@ def execute_autograder_and_construct_feedback():
     pass
 
 
+def configure_questions():
+    """ """
+    return []
+
+
+def configure_and_parse_args():
+    """ """
+    args = {
+        # TODO: Module and package support.
+        ("QUESTIONS",): {
+            "help": "Module or package containing instructor-defined functions",
+            "type": pathlib.Path,
+        },
+        # TODO: Module and package support.
+        ("SOLUTIONS",): {
+            "help": "Module or package containing student solutions",
+            "type": pathlib.Path,
+        },
+        # NOTE: File and directory support.
+        ("-o", "--output"): {
+            "help": "File or directory used to output results",
+            "type": pathlib.Path,
+        }
+    }
+    parser = argparse.ArgumentParser()
+    for options, config in args.items():
+        parser.add_argument(*options, **config)
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    pass
+    print(configure_and_parse_args())
