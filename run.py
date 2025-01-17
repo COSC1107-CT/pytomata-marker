2d7db487ce7c5bb777f59b73b72b01ae5e85c300
Harry Porter <h.porter23@icloud.com>
2025-01-17 14:08:26 +1100

    Feedback output to file done during subprocesses

--- run.py
+++ run.py
@@ -23,13 +23,15 @@ def invoke_autograder_and_output_results():
             yield solution_paths[index :: args.process_count]
 
     args = utilities.construct_and_parse_args()
+    if args.output_directory_path is not None:
+        args.output_directory_path.mkdir(parents=True, exist_ok=True)
     process_context = (args.questions_path, args.output_directory_path)
     results = invoke_autograder(
         partition_solution_paths(), process_context, args.process_count
     )
     if args.output_directory_path is None:
         output = generate_results_and_feedback_output(results)
-        print("\n", "\n\n".join(output), "\n")
+        print("\n", "\n\n".join(output), "\n", sep="")
 
 
 def invoke_autograder(solution_partitions, process_context, process_count):
@@ -54,19 +56,25 @@ def invoke_autograder_over_partition(solution_partition, process_context):
             configure.construct_questions_and_solutions(questions, solutions)
         )
 
-    def output_results_and_feedback_to_files():
+    def output_results_and_feedback_to_files(results):
         """ """
-        pass
+        output = zip(
+            map(lambda result: result[0], results),
+            generate_results_and_feedback_output(results),
+        )
+        for student, feedback in output:
+            with open(output_directory_path / f"{student}.out", "w+") as file:
+                file.write(feedback + "\n")
 
     questions_path, output_directory_path = process_context
     questions = utilities.load_using_path(questions_path, "questions")
-    # TODO: Output to files.
+    results = list(map(invoke_autograder_over_solution, solution_partition))
     if output_directory_path is not None:
-        pass
-    return list(map(invoke_autograder_over_solution, solution_partition))
+        output_results_and_feedback_to_files(results)
+    return results
 
 
-def generate_results_and_feedback_output(results_and_feedback):
+def generate_results_and_feedback_output(results):
     """ """
 
     def generate_output(result):
@@ -80,7 +88,7 @@ def generate_results_and_feedback_output(results_and_feedback):
         question_label, question_value, student_score, feedback = result
         return f"{question_label} | {student_score} / {question_value}\n{feedback}"
 
-    return list(map(generate_output, results_and_feedback))
+    return list(map(generate_output, results))
 
 
 if __name__ == "__main__":
