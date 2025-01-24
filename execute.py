4c8ce1cadfabc56952bb6c4e068627191246290c
Harry Porter <h.porter23@icloud.com>
2025-01-25 10:54:15 +1100

    Updated scaffold for execution control flow

---
+++ execute.py
@@ -0,0 +1,32 @@
+""" """
+
+
+def calculate_and_output_student_results():
+    """ """
+
+    def derive_and_partition_student_solution_files():
+        """ """
+        pass
+
+    pass
+
+
+def calculate_and_output_results_for_student_solution_partition():
+    """ """
+
+    # TODO: Generator.
+    def calculate_results_for_student_solution_partition():
+        """ """
+        pass
+
+    # TODO: File and standard output. Exclusion lock for standard output.
+    def output_student_results():
+        """ """
+        pass
+
+    for _ in calculate_results_for_student_solution_partition():
+        pass
+
+
+if __name__ == "__main__":
+    calculate_and_output_student_results()
