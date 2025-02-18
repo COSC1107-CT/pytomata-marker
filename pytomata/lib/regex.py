921dafec230f4f88d82f4588c218db0c8483d940
Harry Porter <h.porter23@icloud.com>
2025-02-18 18:26:47 +1100

    Cleans up parameters and streamlines conversion of regex to nfa

--- pytomata/lib/regex.py
+++ pytomata/lib/regex.py
@@ -1,4 +1,8 @@
-""" """
+"""
+Module containing library functions for questions involving regular expressions.
+"""
+
+import functools
 
 import automata.fa.nfa as nfa
 
@@ -6,8 +10,6 @@ import pytomata.lib.base as base
 import pytomata.lib.configuration as configuration
 
 
-# TODO: Better student error handling.
-# TODO: Docstrings and usage.
 @configuration.apply_penalty_values()
 def check_words_are_subset_of_regex_language(
     words,
@@ -16,29 +18,41 @@ def check_words_are_subset_of_regex_language(
     question_value,
     incorrect_penalty,
     additional_test_cases=None,
-    regex_input_symbols=None,
 ):
     """
+    Checks that `words` is a subset of the language denoted by `regex`.
+    If `additional_test_cases` are specified,
+    checks that `words` is a subset of each test case regular expression.
+
+    Args:
+        `words`: set of strings denoting words in a language;
+        `regex`: string denoting a regular expression that encodes a language;
+        `additional_test_cases`: additional string regular expressions;
+
+    Raises:
+        `InvalidSymbolError`: reserved characters used as input symbols.
+
     ```python
-    >>> words = {"a", "ab", "bcc"}
-    >>> regex = "a*b*c*"
-    >>> check_words_are_subset_of_language(words, regex, question_value=10)
-    ...
+    >>> words = {"a", "ab", "bcc", "abccc"}
+    >>> solution_regex = "a*b*c*"
+    >>> test_cases = [
+        ("a*b*c?", 0.1, None),
+        ("a*b*c+", 0.2, None),
+    ]
+    >>> check_words_are_subset_of_language(
+        words,
+        solution_regex,
+        question_value=10,
+        additional_test_cases=test_cases,
+    )
     ```
     """
 
     def check_words_are_subset_of_test_case_language(test_case):
-        """ """
-        test_regex, test_value, test_feedback = test_case
-        test_nfa = nfa.NFA.from_regex(
-            test_regex, input_symbols=regex_input_symbols
-        )
-        for word in words:
-            if not test_nfa.accepts_input(word):
-                return False
-        return True
+        test_nfa = nfa.NFA.from_regex(test_case[0])
+        return all(map(lambda word: test_nfa.accepts_input(word), words))
 
-    solution_nfa = nfa.NFA.from_regex(regex, input_symbols=regex_input_symbols)
+    solution_nfa = nfa.NFA.from_regex(regex)
     student_result, student_feedback = (
         _check_words_are_subset_of_automaton_language(
             words, solution_nfa, question_value, incorrect_penalty
@@ -51,11 +65,9 @@ def check_words_are_subset_of_regex_language(
             student_result,
             question_value,
         )
-    student_feedback.extend(test_case_feedback)
-    student_feedback = student_feedback or "Correct!"
-    return base.calculate_final_score(
-        student_result, question_value
-    ), student_feedback
+        student_feedback.extend(test_case_feedback)
+    student_result = base.calculate_final_score(student_result, question_value)
+    return student_result, student_feedback
 
 
 @configuration.apply_penalty_values()
@@ -66,25 +78,37 @@ def check_words_are_subset_of_regex_language_intersection(
     *extra_regexes,
     question_value,
     incorrect_penalty,
-    regex_input_symbols=None,
 ):
-    """ """
-    nfa_1 = nfa.NFA.from_regex(regex_1, input_symbols=regex_input_symbols)
-    nfa_2 = nfa.NFA.from_regex(regex_2, input_symbols=regex_input_symbols)
-    intersection_nfa = nfa_1.intersection(nfa_2)
-    for extra_regex in extra_regexes:
-        extra_nfa = nfa.NFA.from_regex(
-            extra_regex, input_symbols=regex_input_symbols
-        )
-        intersection_nfa = intersection_nfa.intersection(extra_nfa)
+    """
+    Checks that `words` is a subset of the intersection of the languages
+    denoted by an arbitrary set of regular expressions.
+
+    Args:
+        `words`: set of strings denoting words in a language;
+        `regex_1`: …;
+        `regex_2`: …;
+        `extra_regexes`: …
+
+    Raises:
+        `InvalidSymbolError`: reserved characters used as input symbols.
+
+    ```python
+    ```
+    """
+    intersection_nfa = functools.reduce(
+        lambda nfa_1, nfa_2: nfa_1.intersection(nfa_2),
+        map(
+            lambda regex: nfa.NFA.from_regex,
+            (regex_1, regex_2, *extra_regexes),
+        ),
+    )
     student_result, student_feedback = (
         _check_words_are_subset_of_automaton_language(
             words, intersection_nfa, question_value, incorrect_penalty
         )
     )
-    return base.calculate_final_score(
-        student_result, question_value
-    ), student_feedback or "Correct!"
+    student_result = base.calculate_final_score(student_result, question_value)
+    return student_result, student_feedback
 
 
 @configuration.apply_penalty_values()
@@ -92,14 +116,41 @@ def check_words_are_subset_of_language_difference(
     words,
     regex_1,
     regex_2,
-    *,
+    *extra_regexes,
     question_value,
     incorrect_penalty,
-    additional_test_cases=None,
-    regex_input_symbols=None,
 ):
-    """ """
-    pass
+    """
+    Checks that `words` is a subset of the difference of the languages
+    denoted by an arbitrary set of regular expressions.
+    Difference operation is left-associative.
+
+    Args:
+        `words`: set of strings denoting words in a language;
+        `regex_1`: …;
+        `regex_2`: …;
+        `extra_regexes`: …
+
+    Raises:
+        `InvalidSymbolError`: reserved characters used as input symbols.
+
+    ```python
+    ```
+    """
+    difference_nfa = functools.reduce(
+        lambda nfa_1, nfa_2: nfa_1.difference(nfa_2),
+        map(
+            lambda regex: nfa.NFA.from_regex,
+            (regex_1, regex_2, *extra_regexes),
+        ),
+    )
+    student_result, student_feedback = (
+        _check_words_are_subset_of_automaton_language(
+            words, difference_nfa, question_value, incorrect_penalty
+        )
+    )
+    student_result = base.calculate_final_score(student_result, question_value)
+    return student_result, student_feedback
 
 
 def _check_words_are_subset_of_automaton_language(
