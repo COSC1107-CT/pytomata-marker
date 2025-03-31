2c2288c86e770aa7a308bae9d9fc8985588dba4f
Harry Porter <h.porter23@icloud.com>
2025-03-31 17:25:53 +1100

    Fixes regex template marking functions

---
+++ pytomata/library/__init__.py
@@ -0,0 +1,5 @@
+from .regex import (
+    check_regex_acceptance as check_regex_acceptance,
+    check_regex_intersection_acceptance as check_regex_intersection_acceptance,
+    check_regex_difference_acceptance as check_regex_difference_acceptance,
+)
