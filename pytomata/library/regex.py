a7c9a34faa10fded89361cfffce9a951335c8795
Harry Porter <h.porter23@icloud.com>
2025-03-31 17:53:28 +1100

    Converts regexes into DFAs for difference operation

--- pytomata/library/regex.py
+++ pytomata/library/regex.py
@@ -2,6 +2,7 @@
 
 import functools
 
+import automata.fa.dfa as dfa
 import automata.fa.nfa as nfa
 
 
@@ -21,11 +22,15 @@ def check_regex_intersection_acceptance(regexes, student_inputs, *, question_val
 
 def check_regex_difference_acceptance(regexes, student_inputs, *, question_value):
     """ """
+
+    def convert_regex_into_dfa(regex):
+        return dfa.DFA.from_nfa(nfa.NFA.from_regex(regex))
+
     regex, regexes = regexes[0], regexes[1:]
-    difference_nfa = functools.reduce(
-        lambda nfa_1, nfa_2: nfa_1.difference(nfa_2),
-        map(nfa.NFA.from_regex, regexes),
-        nfa.NFA.from_regex(regex),
+    difference_dfa = functools.reduce(
+        lambda dfa_1, dfa_2: dfa_1.difference(dfa_2),
+        map(convert_regex_into_dfa, regexes),
+        convert_regex_into_dfa(regex),
     )
-    accepted = len(list(filter(difference_nfa.accepts_input, student_inputs)))
+    accepted = len(list(filter(difference_dfa.accepts_input, student_inputs)))
     return question_value * (accepted / len(student_inputs))
