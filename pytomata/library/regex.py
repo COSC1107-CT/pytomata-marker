97d0ea469a5df034973562f60b7b69081d8b95a0
ssardina <ssardina@gmail.com>
2025-04-14 07:39:55 +1000

    build doc and feedback string

--- pytomata/library/regex.py
+++ pytomata/library/regex.py
@@ -1,6 +1,7 @@
 """ """
 
 import functools
+from typing import List
 
 import automata.fa.dfa as dfa
 import automata.fa.nfa as nfa
@@ -36,11 +37,28 @@ def check_regex_acceptance(regex, student_inputs, *, question_value):
     return question_value * (accepted / len(student_inputs)), ""
 
 
-def check_regex_intersection_acceptance(regexes, student_inputs, *, question_value):
-    """ """
+def check_regex_intersection_acceptance(regexes: List[str], student_inputs: List[str], *, question_value: float) -> tuple[float, str]:
+    """Check if the intersection of regexes accepts the student inputs strings.
+
+    Args:
+        regexes (List[str]): regular expressions to intersect
+        student_inputs (List[str]): list of string submissions
+        question_value (float): total points the question is worth
+
+    Returns:
+        tuple[float, str]: point attracted + feedback
+    """
     intersection_nfa = nfa.NFA.from_regex("&".join(regexes))
-    accepted = len(list(filter(intersection_nfa.accepts_input, student_inputs)))
-    return question_value * (accepted / len(student_inputs)), ""
+    accepted = list(filter(intersection_nfa.accepts_input, student_inputs))
+    no_accepted = len(accepted)
+
+    rate = (no_accepted / len(student_inputs))
+    if rate == 1:
+        feedback = f"All accepted!"
+    else:
+        rejected = [w for w in student_inputs if w not in accepted]
+        feedback = f"Rejected {(1-rate)*100:.0f}%: {','.join(rejected)}"
+    return question_value * rate, feedback
 
 
 def check_regex_difference_acceptance(regexes, student_inputs, *, question_value):
